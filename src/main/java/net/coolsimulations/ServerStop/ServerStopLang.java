510fca4e6f9d907d63de5f7e3be102682a571842
coolsim <coolsimulations@gmail.com>
2019-01-01 14:45:57 +1000

    Fixed en_ud lang

--- src/main/java/net/coolsimulations/ServerStop/ServerStopLang.java
+++ src/main/java/net/coolsimulations/ServerStop/ServerStopLang.java
@@ -1101,7 +1101,7 @@ public class ServerStopLang {
 		
 		if(text == "serverstop.update.display3") {
 			
-			return "¡sǝɹnʇɐǝɟ ǝɹoɯ ɹoɟ ǝʇɐpdn ǝsɐǝꞁԀ ¡ᘔ˙s0⥝˙⥝ uoᴉsɹǝΛ ʇɟɐɹɔǝuᴉW sʇɹoddns ɹǝᵷuoꞁ ou doʇS ɹǝʌɹǝS";
+			return "¡sǝɹnʇɐǝɟ ǝɹoɯ ɹoɟ ǝʇɐpdn ǝsɐǝꞁԀ ¡ᘔ˙0⥝˙⥝ uoᴉsɹǝΛ ʇɟɐɹɔǝuᴉW sʇɹoddns ɹǝᵷuoꞁ ou doʇS ɹǝʌɹǝS";
 		}
 		
 		if(text == "serverstop.commands.cancel.usage") {
