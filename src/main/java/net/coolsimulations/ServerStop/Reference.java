f105f590b3978223e67a70daca62992ea791ea74
coolsim <coolsimulations@gmail.com>
2021-07-13 12:23:48 +1000

    Updated to 1.15.2

--- src/main/java/net/coolsimulations/ServerStop/Reference.java
+++ src/main/java/net/coolsimulations/ServerStop/Reference.java
@@ -4,10 +4,10 @@ public class Reference {
 	public static final String MOD_ID = "serverstop";
 	public static final String MOD_NAME = "ServerStop";
 	public static final String VERSION = "0.1.0";
-	public static final String ACCEPTED_VERSIONS = "[1.14, 1.14.4]";
+	public static final String ACCEPTED_VERSIONS = "[1.15, 1.15.2]";
 	public static final String CLIENT_PROXY_CLASS = "net.coolsimulations.ServerStop.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "net.coolsimulations.ServerStop.proxy.ServerProxy";
-	public static final String DEPENDENCIES = "required-after:forge@[28.0.55,);";
+	public static final String DEPENDENCIES = "required-after:forge@[31.0.0,);";
 	public static final String SERVERSTOP_CONFIG_FILE = "ServerStop.conf";
 	public static final String CONFIG_CATEGORY_LANG = "lang";
 	public static final String CONFIG_CATEGORY_UTIL = "util";
