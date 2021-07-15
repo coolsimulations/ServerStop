007149ffb021138e77a1332e8257197718f91774
coolsim <coolsimulations@gmail.com>
2021-07-15 11:06:44 +1000

    Initial Commit for 1.9.4

--- src/main/java/net/coolsimulations/ServerStop/ServerStopUpdateHandler.java
+++ src/main/java/net/coolsimulations/ServerStop/ServerStopUpdateHandler.java
@@ -21,7 +21,7 @@ public class ServerStopUpdateHandler {
 	public static void init() {
 
 		try {
-			URL url = new URL("https://coolsimulations.net/mcmods/serverstop/versionchecker110.txt");
+			URL url = new URL("https://coolsimulations.net/mcmods/serverstop/versionchecker19.txt");
 			Scanner s = new Scanner(url.openStream());
 			latestVersion = s.next();
 			s.close();
@@ -30,7 +30,7 @@ public class ServerStopUpdateHandler {
 		}
 
 		try {
-			URL url = new URL("https://coolsimulations.net/mcmods/serverstop/updateinfo110.txt");
+			URL url = new URL("https://coolsimulations.net/mcmods/serverstop/updateinfo19.txt");
 			Scanner s = new Scanner(url.openStream());
 			latestVersionInfo = s.nextLine();
 			s.close();
